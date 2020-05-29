65847b445d6a3c1e85b5467192779f3010518886
Lídia Lodovici <lidia.lodovici@usp.br>
2020-05-29 16:17:47 -0300

    tarefa tentativa 2

--- 04-objetos/0402-prototype/script.js
+++ 04-objetos/0402-prototype/script.js
@@ -12,20 +12,14 @@
   Pessoas.prototype.nomeCompleto = function(){
     return `${this.nome} ${this.sobrenome}`
   }
-
-
   const lidia = new Pessoas('Lídia', 'Fernandes', 31);
-
-
-
-
-
 
 // Liste os métodos acessados por 
 // dados criados com NodeList,
 // HTMLCollection, Document
-
-
+Object.getOwnPropertyNames(NodeList.prototype)
+Object.getOwnPropertyNames(HTMLCollection.prototype)
+Object.getOwnPropertyNames(Document.prototype)
 
 // Liste os construtores dos dados abaixo
 const li = document.querySelector('li');
@@ -39,4 +33,4 @@ li.offsetLeft; // Number
 li.click(); // undefined
 
 // Qual o construtor do dado abaixo:
-li.hidden.constructor.name; 
+li.hidden.constructor.name; //String
